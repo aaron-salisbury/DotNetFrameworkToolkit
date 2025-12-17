415535a32744860036c255c45d99da98d6575426
Aaron Salisbury <aaron.salisbury@live.com>
2025-12-16 20:20:39 -0600

    Create a type that facilitates a static, safe, IServiceProvider.

---
+++ src/DotNetFrameworkToolkit/Modules/DependencyInjection/Implementation/Ioc.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Threading;
+
+namespace DotNetFrameworkToolkit.Modules.DependencyInjection;
+
+/// <summary>
+/// A type that facilitates the use of the <see cref="IServiceProvider"/> type.
+/// The <see cref="Ioc"/> provides the ability to configure services in a singleton, thread-safe
+/// service provider instance, which can then be used to resolve service instances.
+/// </summary>
+public sealed class Ioc : IServiceProvider
+{
+    /// <summary>
+    /// Gets the default <see cref="Ioc"/> instance.
+    /// </summary>
+    public static Ioc Default { get; } = new();
+
+    private volatile IServiceProvider serviceProvider;
+
+    /// <summary>
+    /// Gets the service object of the specified type from the service container.
+    /// </summary>
+    /// <param name="serviceType">The type of service object to get.</param>
+    /// <returns>
+    /// A service object of type <paramref name="serviceType"/>. 
+    /// Returns <c>null</c> if the service is not found.
+    /// </returns>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="serviceType"/> is <see langword="null"/>.</exception>
+    /// <exception cref="InvalidOperationException">Thrown if the service provider has not been configured.</exception>
+    public object GetService(Type serviceType)
+    {
+        if (serviceType is null)
+        {
+            throw new ArgumentNullException(nameof(serviceType));
+        }
+
+        if (this.serviceProvider is null)
+        {
+            ThrowInvalidOperationExceptionForMissingInitialization();
+        }
+
+        return this.serviceProvider.GetService(serviceType);
+    }
+
+    /// <summary>
+    /// Initializes the shared <see cref="IServiceProvider"/> instance.
+    /// </summary>
+    /// <param name="serviceProvider">The input <see cref="IServiceProvider"/> instance to use.</param>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="serviceProvider"/> is <see langword="null"/>.</exception>
+    public void ConfigureServices(IServiceProvider serviceProvider)
+    {
+        if (serviceProvider is null)
+        {
+            throw new ArgumentNullException(nameof(serviceProvider));
+        }
+
+        IServiceProvider oldServices = Interlocked.CompareExchange(ref this.serviceProvider, serviceProvider, null);
+
+        if (oldServices is not null)
+        {
+            ThrowInvalidOperationExceptionForRepeatedConfiguration();
+        }
+    }
+
+    /// <summary>
+    /// Throws an <see cref="InvalidOperationException"/> when the <see cref="IServiceProvider"/> property is used before initialization.
+    /// </summary>
+    private static void ThrowInvalidOperationExceptionForMissingInitialization()
+    {
+        throw new InvalidOperationException("The service provider has not been configured yet.");
+    }
+
+    /// <summary>
+    /// Throws an <see cref="InvalidOperationException"/> when a configuration is attempted more than once.
+    /// </summary>
+    private static void ThrowInvalidOperationExceptionForRepeatedConfiguration()
+    {
+        throw new InvalidOperationException("The default service provider has already been configured.");
+    }
+}
